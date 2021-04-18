744f71c95b19b6e44a1dfbc612d325bf5ae37c58
ixxxMrAnderson <chtvii@sjtu.edu.cn>
2021-04-18 09:06:25 +0800

    Do not code gentle into that good night

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) throws Exception{
 
-        String file_name = "./testcases/sema/class-package/class-2.mx";
+        String file_name = "./testcases/sema/misc-package/misc-36.mx";
 //        InputStream input = new FileInputStream(file_name);
         InputStream input = System.in;
 //
