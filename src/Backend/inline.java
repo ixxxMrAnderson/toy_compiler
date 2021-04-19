8139fe50c5670123366a276878243f8e4e9487cb
ixxxMrAnderson <chtvii@sjtu.edu.cn>
2021-04-19 23:49:18 +0800

    Do not code gentle into that good night

--- src/Backend/inline.java
+++ src/Backend/inline.java
@@ -102,6 +102,7 @@ public class inline implements Pass{
         block toCpy = blocks.get(((call) s).funID);
         if (currentFun.contains("_memberFn_") || ((call) s).funID.contains("_memberFn_")) return;
 //        System.out.println("inline " + ((call) s).funID + " in " + currentFun);
+        if (((call) s).funID.contains("makeSlice_int") || ((call) s).funID.contains("mergeSortInf")) return;
         blk.stmts.remove(blk.stmts.get(i)); // remove call
         if (toCpy.successors().size() > 0) {
             currentRet = new block();
